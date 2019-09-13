78f36321dc97aa17a63c86c59b1a9121fc8e0c6a
Hannah Jacoby <hjacoby@grubhub.com>
2019-09-12 19:34:57 -0600

    routing set

---
+++ FriendFinder/routing/apiRoutes.js
@@ -0,0 +1,29 @@
+// LOAD DATA
+var friendData= require("../data/friendData");
+
+// ROUTING
+
+module.exports = function(app) {
+
+// API GET Request
+app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
+
+// API POST Requests
+app.post("/api/friends", function(req, res) {  
+
+    if (friendData.length < 5) {
+        friendData.push(req.body);
+        res.json(true);
+      }
+      
+    });
+
+    app.post("/api/clear", function(req, res) {
+        // Empty out the arrays of data
+        friendData.length = 0;
+    
+        res.json({ ok: true });
+      });
+    };
