d79bc66b1bf1e51d6cea2be147361ec5c565351a
Hannah Jacoby <hjacoby@grubhub.com>
2019-09-15 12:44:43 -0600

    friend finder

--- FriendFinder/app/routing/apiRoutes.js
+++ FriendFinder/app/routing/apiRoutes.js
@@ -1,53 +1,40 @@
 // Load Data
-var friendData= require("../data/friends");
-var friendArray= require("../data/friends");
+var friendData = require("../data/friends");
+var friendArray = require("../data/friends");
 
 // Routing
 module.exports = function(app) {
-
-// // API GET Request
-app.get("/api/friends", function(req, res) {
+  // // API GET Request
+  app.get("/api/friends", function(req, res) {
     res.json(friendData);
   });
 
   app.post("/api/friends", function(req, res) {
-    console.log(req.body);
-    console.log(friendData);
-      
-var myScores = [
-  5,
-  1,
-  4,
-  4,
-  5,
-  1,
-  2,
-  5,
-  4,
-  1
-]
-var bestFriend = {
-  name: "",
-  photo: "",
-  diff: 100
-}
+    // console.log(req.body);
+    // console.log(friendData);
 
-    console.log(friendArray[0])
-    for (var i = 0; i<friendArray.length; i++) {
-      var totalDifference = 0
-      for (var j = 0; j<friendArray[i].scores.length; j++) {
-          var a = friendArray[i].scores[j]
-          var b = myScores[j]
-          totalDifference += Math.abs(parseInt(a) - b)
+    var myScores = [5, 1, 4, 4, 5, 1, 2, 5, 4, 1];
+    var bestFriend = {
+      name: "",
+      photo: "",
+      diff: 100
+    };
+
+    for (var i = 0; i < friendArray.length; i++) {
+      var totalDifference = 100;
+      for (var j = 0; j < friendArray[i].scores.length; j++) {
+        var a = friendArray[i].scores[j];
+        var b = myScores[j];
+        totalDifference += Math.abs(parseInt(a) - b);
       }
-      if(bestFriend.diff > totalDifference){
-          bestFriend.name = friendArray[i].name
-          bestFriend.photo = friendArray[i].photo
-          bestFriend.diff = totalDifference
+      if (totalDifference < bestFriend.diff) {
+        bestFriend.name = friendArray[i].name;
+        bestFriend.photo = friendArray[i].photo;
+        bestFriend.diff = totalDifference;
+        friendData.push(req.body);
+        res.json(true);
       }
     }
-    console.log(bestFriend)
-
+    console.log(bestFriend);
   });
- 
-  };
+};
