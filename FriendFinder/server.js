704017585bfcca9f64ae421e35991b2b083bb43f
Hannah Jacoby <hjacoby@grubhub.com>
2019-09-16 17:08:00 -0600

    bug work

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -19,6 +19,6 @@ require("./app/routing/htmlRoutes")(app);
 
 
 // Listener
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
