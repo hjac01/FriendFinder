9036a1bb9df79c38a090e8a4f5c5a0b77725afc9
Hannah Jacoby <hjacoby@grubhub.com>
2019-09-16 16:48:56 -0600

    testing

--- FriendFinder/server.js
+++ FriendFinder/server.js
@@ -6,7 +6,7 @@ var path = require("path");
 var app = express();
 
 // Set port
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
